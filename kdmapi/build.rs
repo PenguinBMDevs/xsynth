e47cb21f0f5efeaa3750544bee31ac7e1c711b4d
节能降耗 <3347830431@qq.com>
2026-05-02 02:08:35 +0800

    为特定插件而精简功能

--- kdmapi/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    if cfg!(windows) {
-        // Building on Windows
-        println!("cargo:rustc-cdylib-link-arg=/DEF:kdmapi/Ordinals.def")
-    } else if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        // Cross building for Windows
-        println!("cargo:rustc-cdylib-link-arg=kdmapi/Ordinals.def")
-    }
-}
